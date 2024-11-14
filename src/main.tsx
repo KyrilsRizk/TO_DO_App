011a4b30646365b6b1a42a76e0116506bef7975e
kyrolis <kerodex224@gmail.com>
2024-11-14 17:17:59 +0200

    second done

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from './App.tsx'
 import './index.css'
 import TodoContextProvider from './contexts/TodoContextProvider.tsx'
 import { KindeProvider } from '@kinde-oss/kinde-auth-react'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<KindeProvider
 		clientId='9718138185e44922a4b478cd2f93955a'
@@ -17,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 				? 'https://todoapp-wheat-eight.vercel.app'
 				: 'http://localhost:5173'
 		}
-		isDangerouslyUseLocalStorage(ture)
+		isDangerouslyUseLocalStorage={true}
 	>
 		<TodoContextProvider>
 			<App />
